ef51297bd819895df4f4198acd42437e05aeeab9
Shift Poster <86fullers_secrets@icloud.com>
2026-01-27 15:47:06 -0500

    adding loguru cmd to exports if loguru available

--- src/overhead_cmd2/__init__.py
+++ src/overhead_cmd2/__init__.py
@@ -5,3 +5,14 @@ __all__ = [
     "strip_cmd2_wrapped",
     "with_model_parser",
 ]
+
+try:
+    from overhead_cmd2._loguru import (  # noqa: F401
+        FILE_ONLY_KEY,
+        LoguruCmd,
+        file_only_filter,
+    )
+
+    __all__.extend(["LoguruCmd", "file_only_filter", "FILE_ONLY_KEY"])
+except ImportError:
+    pass
